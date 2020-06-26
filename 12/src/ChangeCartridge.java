2eba6eef22b0e36e2e75758ab14a6c382cc6992b
m4s4y4 <54763094+m4s4y4@users.noreply.github.com>
2020-06-26 17:04:23 +0900

    add calc program

--- 12/src/ChangeCartridge.java
+++ 12/src/ChangeCartridge.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class ChangeCartridge {
-    Color foo = new Color();
+    ArrayList<ArrayList<String>> putcartridge = new ArrayList<ArrayList<String>>();
+    ArrayList<String> Innerputcartridge = new ArrayList<String>();
+    ChangeCartridge(){
+    }
+    /**
+     * InnerChangeCartridge
+     */
+    public void InnerChangeCartridge() {
+        if (putcartridge.get(i).get(0) == 0) {
+
+            if (putcartridge.get(i).get(1)) {
+                putcartridge.get(i).set(3, String.valueOf(Integer.parseInt(putcartridge.get(i).get(4))-1));
+
+                if(putcartridge.get(i).get(3) == "0"){
+                    putcartridge.get(i).set(4,putcartridge.get(i).get(3));
+                }
+            }
+        } else {
+            Innerputcartridge.add("pen");
+            Innerputcartridge.add("color_num");
+            Innerputcartridge.add("pen_limit");
+            Innerputcartridge.add("pen_limit_now");
+            Innerputcartridge.add("0");
+            putcartridge.add(Innerputcartridge);
+        }
+    }
+
 }
